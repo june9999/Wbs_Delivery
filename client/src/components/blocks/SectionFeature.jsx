47921d173535c588bfcf3e112628d93d4986fd81
j-grosse <pixelpause@gmx.de>
2023-08-31 15:30:40 +0200

    styled components

--- client/src/components/blocks/SectionFeature.jsx
+++ client/src/components/blocks/SectionFeature.jsx
@@ -6,11 +6,6 @@ const SectionFeature = () => {
     <section className="bg-white dark:bg-gray-900">
       <div className="flexpy-2 px-4 mx-auto max-w-screen-xl sm:py-8 lg:px-6">
         <div className="flex gap-8 mb-16">
-          <img
-            className=" w-60 h-70 rounded-lg"
-            src={catBasket1}
-            alt="cat in bike basket"
-          />
           <div className="max-w-screen-md mb-8 lg:mb-16">
             <div>
               <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
@@ -23,6 +18,11 @@ const SectionFeature = () => {
               </p>
             </div>
           </div>
+          <img
+            className=" w-60 h-70 rounded-lg"
+            src={catBasket1}
+            alt="cat in bike basket"
+          />
         </div>
 
         <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
