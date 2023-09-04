02a2bc94d202bdeb058e85bec98505d3945ed0da
Anusha Abhilash <anusha.vinaya97@gmail.com>
2023-09-04 15:05:56 +0200

    payment update

--- client/src/pages/NewOrder.jsx
+++ client/src/pages/NewOrder.jsx
@@ -345,7 +345,7 @@ const [paid,setPaid]=useState(false)
         )}
         {!checkout && (
           <button
-            onClick={payment}
+            // onClick={payment}
             className=" inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800  "
           >
             Payment
