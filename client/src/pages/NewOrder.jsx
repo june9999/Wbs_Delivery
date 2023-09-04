15f61260c2bcb85a7de92ad9d5e64d6203293803
WENJUN <vesper0@163.com>
2023-09-04 10:47:35 +0200

    fix the conflict in Neworder

--- client/src/pages/NewOrder.jsx
+++ client/src/pages/NewOrder.jsx
@@ -5,9 +5,9 @@ import { AuthContext } from "../context/Auth";
 import Checkout from "./Checkout";
 import socket from "../../socket/socket";
 
-import axios from '../axiosInstance';
-import ProjMap from '../components/ProjMap';
-import PayModal from '../components/PayModal';
+import axios from "../axiosInstance";
+import ProjMap from "../components/ProjMap";
+import PayModal from "../components/PayModal";
 
 const NewOrder = () => {
   const { user } = useContext(AuthContext);
@@ -51,8 +51,6 @@ const NewOrder = () => {
           <p>{order.paid}</p>
         --  <p>{order.timestamps}</p>
   */
-
-
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -350,22 +348,28 @@ const NewOrder = () => {
           </button>
         )}
       </div>
-      
-    </section>
-    </> ) : (
+
+      {/* </section> */}
+      {/* </> ) : (
 
     <Checkout price={price} distance={distance}/> 
     
     
    
-    )}
+    )} */}
 
-<div className="flex items-center justify-center" >
-{ checkout && <button onClick={(()=>setCheckout(false))}  className=" inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800  ">Checkout</button>}
-{ !checkout && <PayModal/>}
-{/* { !checkout && <button onClick={handlePayment}   className=" inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800  ">Payment</button>} */}
-</div>
-    
+      <div className="flex items-center justify-center">
+        {checkout && (
+          <button
+            onClick={() => setCheckout(false)}
+            className=" inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800  "
+          >
+            Checkout
+          </button>
+        )}
+        {!checkout && <PayModal />}
+        {/* { !checkout && <button onClick={handlePayment}   className=" inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800  ">Payment</button>} */}
+      </div>
     </>
   );
 };
